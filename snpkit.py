ef00f8c5a6e72c822b756ce71e165d639dbf1ce0
Varuna Bamunusinghe <varuna@selfdecode.com>
2020-03-26 12:17:52 +0530

    Add command to get from fasta.

--- snpkit.py
+++ snpkit.py
@@ -2,7 +2,7 @@ import tempfile
 
 import click
 
-from src.snpkit import generate_bedfile, generate_fasta
+from src.snpkit import generate_bedfile, generate_fasta, generate_snp_reference
 
 
 @click.group()
@@ -17,6 +17,15 @@ def snpkit():
 def bedfile(snp_list: str, output: str):
     """Create bed file from snp list."""
     generate_bedfile(snp_list, output)
+
+
+@snpkit.command()
+@click.option('--snp-list', required=True, help='Snp list file')
+@click.option('--fasta', required=True, help='Tabbed fasta file')
+@click.option('--limit', is_flag=True, help='Limit to unknow SNPs')
+def gen_fromfasta(snp_list: str, fasta: str, limit: bool):
+    """Generate snp reference file from fasta bed."""
+    generate_snp_reference(snp_list, fasta, limited=limit)
 
 
 @snpkit.command()
--- src/snpkit.py
+++ src/snpkit.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 def generate_bedfile(snp_list, output):
-    snps = _extract_snps(snp_list)
+    snps = _extract_bedable_snps(snp_list)
     snps = _sort_snps_by_chrom_pos(snps)
     _output_to_bedfile(output, snps)
 
@@ -26,12 +26,39 @@ def generate_fasta(bed_file, fasta_file, output_file):
     )
 
 
+def generate_snp_reference(snp_list, fasta_file, limited=False):
+    with gzip.open(snp_list, mode='rt') as fin:
+        reader = csv.reader(fin, delimiter='\t')
+        next(reader)  # skip header
+
+        snps = {}
+        for rsid, chrom, pos, genotype, *other in reader:
+            if limited and genotype:
+                # skip known genotypes in limited version
+                continue
+            snps[rsid] = (chrom, pos, genotype, *other)
+
+    with open(fasta_file) as fin:
+        reader = csv.reader(fin, delimiter='\t')
+
+        for name, allele in reader:
+            rsid = name.split(':')[0]
+            if rsid in snps:
+                chrom, pos, _, *other = snps[rsid]
+                print('\t'.join([rsid, chrom, pos, allele + allele, *other]))
+                del snps[rsid]
+
+        if not limited:  # don't add remaining snps in limited version
+            for rsid in snps.keys():
+                print('\t'.join([rsid, *snps[rsid]]))
+
+
 def _sort_snps_by_chrom_pos(snps):
     snps = sorted(snps)
     return snps
 
 
-def _extract_snps(snp_list):
+def _extract_bedable_snps(snp_list, skip_known=False):
     with gzip.open(snp_list, mode='rt') as fin:
         reader = csv.reader(fin, delimiter='\t')
         next(reader)  # skip header
@@ -43,8 +70,9 @@ def _extract_snps(snp_list):
                 continue
 
             # skp if genotype is available
-            if len(line) >= 4 and len(line[3]) == 2:
-                continue
+            if skip_known:
+                if len(line) >= 4 and len(line[3]) == 2:
+                    continue
 
             rsid = line[0]
             chrom = int(line[1])
